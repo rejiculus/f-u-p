1fa9108433edb322941bc3749636738387e97c0a
rejiculus <73344097+rejiculus@users.noreply.github.com>
2020-12-05 20:45:38 +0200

    Created cameraScript class

---
+++ Assets/scripts/cameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _targetObject;
+
+    private Vector3 _offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _offset = transform.position - _targetObject.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = _targetObject.position + _offset;
+    }
+}
