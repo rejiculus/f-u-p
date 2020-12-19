e846a5c0df780ad9a779d939a84992bca9899bba
rejiculus <73344097+rejiculus@users.noreply.github.com>
2020-12-19 17:08:15 +0200

    add platform script

---
+++ Assets/scripts/Platform.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject, 1.0f);
+        }
+    }
+}
