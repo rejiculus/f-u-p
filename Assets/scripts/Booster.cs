6957ac75e2e2cf62e89ebf5ea0d1299a31e756a1
rejiculus <73344097+rejiculus@users.noreply.github.com>
2020-12-19 17:09:45 +0200

    add booster script

---
+++ Assets/scripts/Booster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class Booster : MonoBehaviour
+{
+    [SerializeField]
+    private int _boost = 300;
+
+    [SerializeField]
+    private Rigidbody _gameObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {           
+            other.GetComponent<Rigidbody>().AddForce(transform.up * _boost);
+        }
+    }
+}
