47b13e2608657b9482464d3799c9ece9f2de2a6d
rejiculus <73344097+rejiculus@users.noreply.github.com>
2020-12-19 17:07:13 +0200

    add switcher script

---
+++ Assets/scripts/Switcher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switcher : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _objectToSwitch;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _objectToSwitch.SetActive(!_objectToSwitch.activeSelf);          
+        }
+    }
+}
