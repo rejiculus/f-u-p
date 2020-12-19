830681cf5c6f9f730fc1c5ebcfc0ae428c4882e3
rejiculus <73344097+rejiculus@users.noreply.github.com>
2020-12-19 17:09:07 +0200

    add jump to movement script

--- Assets/scripts/movemantScript.cs
+++ Assets/scripts/movemantScript.cs
@@ -8,18 +8,28 @@ public class movemantScript : MonoBehaviour
     private float _speed;
 
     [SerializeField]
+    private float _jumpForce;
+
+    [SerializeField]
     private Rigidbody _objectBody;
+
+    private float _jumpRayLangth;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _jumpRayLangth = transform.localScale.y / 2 + 0.1f;
     }
 
     void FixedUpdate() {
         float forward = Input.GetAxisRaw("Vertical");
         float right = Input.GetAxisRaw("Horizontal");
         _objectBody.AddForce(new Vector3(right,0,forward) * _speed * Time.deltaTime);
-    
+
+        if (Input.GetKey(KeyCode.Space) && Physics.Raycast(transform.position, Vector3.down, _jumpRayLangth))
+        {
+            _objectBody.AddForce(Vector3.up * _jumpForce);
+        }
+        
     }
 
     // Update is called once per frame
