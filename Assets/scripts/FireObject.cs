b2077f024222e9ad4d78c81d8a4803a0fe1a8462
rejiculus <73344097+rejiculus@users.noreply.github.com>
2020-12-19 20:29:55 +0200

    xyi

---
+++ Assets/scripts/FireObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class FireObject : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
---
+++ Assets/scripts/PortalScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform pointToAppear;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = pointToAppear.position;
+        }
+    }
+}
